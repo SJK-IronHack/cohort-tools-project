baa5ad529331b48178948434daa57bc95d4a7cf1
Noelia <noeliairanzoribera@gmail.com>
2024-01-16 15:32:54 +0100

    added CORS

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+const cors = require("cors");
 
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 
-// MIDDLEWARE
+// MIDDLEWARE (aka Express.js functions)
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
