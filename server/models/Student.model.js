d4a36ee12b7eb6d1e7c2f99d0b054f849b4b0170
Noelia <noeliairanzoribera@gmail.com>
2024-01-16 17:08:07 +0100

    ObjectId working correctly

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema;
+const { Schema, Types: { ObjectId } } = mongoose;
+
 
 const studentSchema = new Schema({
     firstName: { type: String, required: true },
@@ -7,11 +8,11 @@ const studentSchema = new Schema({
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true },
     linkedinUrl: { type: String, default: "" },
-    languages: {type: Array, enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]},
+    languages: { type: [{ type: String, enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"] }]},
     program: { type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
     background: { type: String, default: "" },
     image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-    cohort: ObjectId,
+    cohort: { type: ObjectId, ref: 'Cohort' },
     projects: Array
 });
 
