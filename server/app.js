1a556eaa6b16a9d7a876d67568a745b5453efa0e
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-17 17:01:33 +0100

    added cohort routesa

--- server/app.js
+++ server/app.js
@@ -128,28 +128,80 @@ app.delete("/api/students/:studentId", async (req, res) => {
 
 
 
-// COHORT ROUTES
 
-// 2.3.1. GET - Returns all the cohorts from the static students array
-app.get("/api/cohortsFromJson", (req, res) => {
-   res.json(cohorts);
-});
+//COHORT ROUTES
+// // 2.3.1. GET - Returns all the cohorts from the static students array
+// app.get("/api/cohortsFromJson", (req, res) => {
+//   res.json(cohorts);
+// });
 
+  // 2.3.8 POST /api/cohorts - Creates a new cohort
+  app.post("/api/cohorts", async (req, res) => {
+    const payload = req.body
+    try {
+      const newCohort = await Cohort.create(payload);
+      res.status(201).json(newCohort);
+    }
+    catch (error) {
+      console.log(error)
+      res.status(500).json({ error, message: "Failed to create a Cohort" })
+    }
+  })
+  // 2.3.9. GET /api/cohorts - Retrieves all of the cohorts in the database collection
 
-// 2.3.2. 
-
-
-
-// 2.3.3.  GET  /cohorts - Retrieves all cohorts from the database
-app.get("/api/students", async (req, res) => {
+app.get("/api/cohorts", async (req, res) => {
   try {
     const allCohorts = await Cohort.find()
     res.status(200).json(allCohorts)
   }
   catch (error) {
-    res.status(500).json({ error, message: "Failed to get all cohorts." })
+    res.status(500).json({ error, message: "Failed to get all cohorts" })
   }
 })
+
+  // 2.3.10 GET /api/cohorts/:cohortId - Retrieves a specific cohort by id
+  app.get("/api/students/:cohortId", async (req, res) => {
+    try {
+      const cohortId = req.params.cohortId;
+      const oneCohort = await Cohort.findById(cohortId);
+      res.status(200).json(oneCohort);
+    } catch (error) {
+      res.status(500).json({ error, message: "failed to get the cohort" });
+    }
+  });
+  // 2.3.11. PUT /api/cohorts/:cohortId - Updates a specific cohort by id
+  app.put("/api/cohorts/:cohortId", async (req, res) => {
+    try {
+      const cohortId = req.params.cohortId;
+      const updatedCohortData = req.body; 
+  
+      const updatedCohort = await Cohort.findByIdAndUpdate(cohortId, updatedCohortData, { new: true });
+  
+      if (!updatedCohort) {
+        return res.status(404).json({ message: "Cohort not found" });
+      }
+  
+      res.status(200).json(updatedCohort);
+    } catch (error) {
+      res.status(500).json({ error, message: "Failed to update the cohort" });
+    }
+  });
+  // 2.3.12 DELETE /api/cohorts/:cohortId - Deletes a specific cohort by id
+  app.delete("/api/cohorts/:cohortId", async (req, res) => {
+    try {
+      const studentId = req.params.cohortId;
+      const deletedCohort = await Cohort.findByIdAndDelete(cohortId);
+      if (!deletedCohort) {
+        return res.status(404).json({ error, message: "Cohort not found." })
+      }
+      res.status(200).send();
+    } catch (error) {
+      res.status(500).json({ error, message: "Failed to delete cohort." });
+    }
+  });
+
+
+
 
 
 
